a3826d9aaa907972c77e7bcd1ae9d9caf21de6a7
tsyupro <vtsyupro@gmail.com>
2024-02-07 00:18:35 +0200

    add logic JS

--- js/script.js
+++ js/script.js
@@ -1,71 +1,98 @@
 document.addEventListener("DOMContentLoaded", function () {
-    swiper = new Swiper('.swiper', {
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-            dynamicBullets: true,
-        },
-        slidesPerView: 4,
-        spaceBetween: 10,
+    const swiper = new Swiper(".swiper", {
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+      },
+      slidesPerView: 4,
+      spaceBetween: 10,
     });
-
+  
     let vimeoVideos = [
-        "https://player.vimeo.com/889793639",
-        "https://player.vimeo.com/video/824804225",
-        "https://player.vimeo.com/video/824804225",
-        "https://player.vimeo.com/video/824804225",
-        "https://player.vimeo.com/video/824804225",
-        "https://player.vimeo.com/video/824804225",
-        "https://player.vimeo.com/video/824804225",
-        "https://player.vimeo.com/video/824804225",
-        "https://player.vimeo.com/video/824804225",
-        "https://player.vimeo.com/video/824804225",
+      "909556066",
+      "889793639",
+      "909556066", 
+      "885725676", 
+      "850096821", 
+      "825147634", 
+      "825137865", 
+      "706958109", 
+      "668990005", 
+      "632819572", 
+      "527943135", 
+      "538276543", 
     ];
-
-    let swiperWrapper = document.querySelector('.swiper-wrapper');
-    vimeoVideos.forEach(function (videoUrl) {
-        let slide = document.createElement('div');
-        slide.classList.add('swiper-slide');
-
-        let iframeElement = document.createElement('iframe');
-        let videoId = getVimeoVideoId(videoUrl);
-        iframeElement.src = "https://player.vimeo.com/video/" + videoId;
-        iframeElement.frameborder = "0";
-        iframeElement.allowfullscreen = true;
-
-        iframeElement.onclick = function() {
-            openModal("https://player.vimeo.com/video/" + videoId);
-        };
-
-        slide.appendChild(iframeElement);
-        swiperWrapper.appendChild(slide);
+  
+    vimeoVideos.forEach(function (videoId) {
+      fetch(`https://api.vimeo.com/videos/${videoId}`, {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer TOKEN",
+        },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then((data) => {
+          if (data && data.pictures && data.pictures.sizes) {
+            const slide = document.createElement("div");
+            slide.classList.add("swiper-slide");
+  
+            const iframePlaceholder = document.createElement("img");
+            const thumbnailIndex = 3; 
+            if (data.pictures.sizes[thumbnailIndex]) {
+              iframePlaceholder.src = data.pictures.sizes[thumbnailIndex].link;
+            } else {
+              iframePlaceholder.src = "path_to_default_image";
+            }
+  
+            iframePlaceholder.alt = "Video thumbnail";
+            iframePlaceholder.style.width = "100%";
+  
+            slide.appendChild(iframePlaceholder);
+            document.querySelector(".swiper-wrapper").appendChild(slide);
+  
+            slide.addEventListener("click", function () {
+              openModal(`https://player.vimeo.com/video/${videoId}`);
+            });
+          } else {
+            console.error("Missing video data or pictures sizes.");
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching data: ", error);
+          if (error.message.includes("401")) {
+            console.error(
+              "Unauthorized: Access Token may be invalid or expired."
+            );
+          }
+        });
     });
-});
-
-
-function openModal(videoUrl) {
+  });
+  
+  function openModal(videoUrl) {
     let modal = document.getElementById("myModal");
     let videoElement = document.getElementById("popupVideo");
     videoElement.src = videoUrl;
     modal.style.display = "block";
-}
-
-function closeModal() {
-    let modal = document.getElementById("myModal");
-    let videoElement = document.getElementById("popupVideo");
-    videoElement.pause();
+  }
+  
+  document.querySelector(".close").addEventListener("click", function () {
+    closeModal();
+  });
+  
+  function closeModal() {
+    const modal = document.getElementById("myModal");
+    const videoElement = document.getElementById("popupVideo");
+    videoElement.src = "";
     modal.style.display = "none";
-}
-
-function getVimeoVideoId(url) {
-    var regex = /(?:https?:\/\/)?(?:www\.)?(?:player\.)?vimeo\.com\/(?:video\/)?(\d+)/;
-    var match = url.match(regex);
-    if (match && match.length > 1) {
-        return match[1];
-    }
-    return null;
-}
+  }
+  
